32cfc1f0b40e05b7a73505b1e24041a36c76a62f
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2024-05-15 16:22:28 -0300

    Add: arquivo main e .csproj

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using estacionamento.models;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// inicia variaveis
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
+
+Console.WriteLine("Digite o preço inicial:");
+Decimal.TryParse(Console.ReadLine(), out precoInicial);
+
+Console.WriteLine("Agora digite o preço por hora:");
+Decimal.TryParse(Console.ReadLine(), out precoPorHora);
+
+// instancia a classe Estacionamento, já com os valores informados
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+// Realiza o loop do menu
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+
+        case "2":
+            es.RemoverVeiculoManual();
+            break;
+
+        case "3":
+            es.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
